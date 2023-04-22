df763c50dc6816d27574555b43260348f9970cf8
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-04-22 21:48:36 +0800

    updated sql create table script

--- backend/initpgdb.sql
+++ backend/initpgdb.sql
@@ -1,5 +1,18 @@
-CREATE TABLE videos (
+CREATE TABLE IF NOT EXISTS User (
+    username VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS YouTubeCredential (
     id SERIAL PRIMARY KEY,
-    url VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+    username VARCHAR(255) REFERENCES User(username),
+    api_key VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS YouTubeVideo (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) REFERENCES User(username),
+    video_link VARCHAR(255) NOT NULL,
+    published_at TIMESTAMP NOT NULL
 );
