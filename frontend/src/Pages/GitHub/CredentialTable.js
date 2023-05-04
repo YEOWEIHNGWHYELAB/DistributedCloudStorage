ad1a0169586fb3ddbb0b5abb75213aca4b254723
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-05-04 16:31:33 +0800

    sorting tables by descending or ascending order

--- frontend/src/Pages/GitHub/CredentialTable.js
+++ frontend/src/Pages/GitHub/CredentialTable.js
@@ -13,6 +13,9 @@ function CredentialsTable() {
         getResourceList();
     }, [getResourceList]);
 
+    const [sortColumn, setSortColumn] = useState('username');
+    const [sortDirection, setSortDirection] = useState('asc');
+
     const credentials = [
         {
             username: "user1",
@@ -31,72 +34,106 @@ function CredentialsTable() {
         },
     ];
 
+    const handleSort = (column) => {
+        if (column === sortColumn) {
+            // if clicking on the current sort column, toggle the sort direction
+            setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+        } else {
+            // if clicking on a new sort column, set the sort column and direction to default values
+            setSortColumn(column);
+            setSortDirection("asc");
+        }
+    };
+
+    // sort the credentials array based on the current sort column and direction
+    const sortedCredentials = [...credentials].sort((a, b) => {
+        let comparison = 0;
+        if (a[sortColumn] > b[sortColumn]) {
+            comparison = 1;
+        } else if (a[sortColumn] < b[sortColumn]) {
+            comparison = -1;
+        }
+        return sortDirection === "asc" ? comparison : comparison * -1;
+    });
+
     const handleCopyToken = (token) => {
         navigator.clipboard.writeText(token);
     };
 
     return (
-        <div style={{ margin: "0 auto", padding: "20px" }}>
-            <h3> My Github Credentials: </h3>
-
-            <Table style={{width: "100%"}}>
-                <thead>
-                    <tr>
-                        <th
-                            style={{
-                                width: "25%",
-                                border: "2px solid red",
-                                textAlign: "left",
-                            }}
-                        >
-                            Username
-                        </th>
-                        <th
-                            style={{
-                                width: "25%",
-                                border: "2px solid red",
-                                textAlign: "left",
-                            }}
-                        >
-                            Email
-                        </th>
-                        <th
-                            style={{
-                                width: "50%",
-                                border: "2px solid red",
-                                textAlign: "left",
-                            }}
-                        >
-                            Personal Access Token 📋
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {credentials.map((credential, index) => (
-                        <tr key={index}>
-                            <td style={{ border: "2px solid" }}>
-                                {credential.username}
-                            </td>
-                            <td style={{ border: "2px solid" }}>
-                                {credential.email}
-                            </td>
-                            <td
+        <div className="credentials-table-wrapper">
+            <h1>Github Credentials:</h1>
+            <div className="table-container">
+                <Table striped bordered hover responsive>
+                    <thead>
+                        <tr>
+                            <th
                                 style={{
-                                    border: "2px solid",
+                                    width: "25%",
+                                    border: "1px solid black",
                                     cursor: "pointer",
                                 }}
-                                onClick={() =>
-                                    handleCopyToken(
-                                        credential.personalAccessToken
-                                    )
-                                }
+                                onClick={() => handleSort("username")}
                             >
-                                {credential.personalAccessToken}
-                            </td>
+                                Username{" "}
+                                {sortColumn === "username" && (
+                                    <i
+                                        className={`fas fa-sort-${sortDirection}`}
+                                    />
+                                )}
+                            </th>
+                            <th
+                                style={{
+                                    width: "50%",
+                                    border: "1px solid black",
+                                    cursor: "pointer",
+                                }}
+                                onClick={() => handleSort("email")}
+                            >
+                                Email{" "}
+                                {sortColumn === "email" && (
+                                    <i
+                                        className={`fas fa-sort-${sortDirection}`}
+                                    />
+                                )}
+                            </th>
+                            <th
+                                style={{
+                                    width: "25%",
+                                    border: "1px solid black",
+                                }}
+                            >
+                                Personal Access Token
+                            </th>
                         </tr>
-                    ))}
-                </tbody>
-            </Table>
+                    </thead>
+                    <tbody>
+                        {sortedCredentials.map((credential, index) => (
+                            <tr key={index}>
+                                <td style={{ border: "1px solid black" }}>
+                                    {credential.username}
+                                </td>
+                                <td style={{ border: "1px solid black" }}>
+                                    {credential.email}
+                                </td>
+                                <td
+                                    style={{
+                                        border: "1px solid black",
+                                        cursor: "pointer",
+                                    }}
+                                    onClick={() =>
+                                        handleCopyToken(
+                                            credential.personalAccessToken
+                                        )
+                                    }
+                                >
+                                    {credential.personalAccessToken}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
         </div>
     );
 }
