0b0fa2319d223d842f57a71b8b007e3f9079d596
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-05-05 11:47:57 +0800

    Added search window

--- frontend/src/Pages/GitHub/CredentialTable.js
+++ frontend/src/Pages/GitHub/CredentialTable.js
@@ -1,71 +1,70 @@
 import React, { useEffect, useState } from "react";
 import RequestResource from "../../Hooks/RequestResource";
-import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClipboard, faHandPointer } from '@fortawesome/free-solid-svg-icons';
-
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboard, faHandPointer } from "@fortawesome/free-solid-svg-icons";
 
 const StyledTable = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-  margin-top: 24px;
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 24px;
 `;
 
 const StyledHeaderRow = styled.tr`
-  height: 40px;
-  font-weight: bold;
-  font-size: 14px;
-  color: red;
+    height: 40px;
+    font-weight: bold;
+    font-size: 14px;
+    color: red;
 `;
 
 const StyledHeaderCell = styled.th`
-  padding: 8px;
-  text-align: left;
-  cursor: pointer;
-  border: 2px solid #ddd;
+    padding: 8px;
+    text-align: left;
+    cursor: pointer;
+    border: 2px solid #ddd;
 
-  &:hover {
-    background-color: #ddd;
-  }
-
-  &.sortable {
-    position: relative;
-
-    &:after {
-      content: '';
-      display: inline-block;
-      margin-left: 6px;
-      width: 0;
-      height: 0;
-      border-left: 6px solid transparent;
-      border-right: 6px solid transparent;
-      border-top: 6px solid #aaa;
-      position: absolute;
-      top: 50%;
-      transform: translateY(-50%);
-      right: 8px;
+    &:hover {
+        background-color: #ddd;
     }
 
-    &.asc:after {
-      border-top: none;
-      border-bottom: 6px solid #aaa;
-    }
+    &.sortable {
+        position: relative;
 
-    &.desc:after {
-      border-top: 6px solid #aaa;
-      border-bottom: none;
+        &:after {
+            content: "";
+            display: inline-block;
+            margin-left: 6px;
+            width: 0;
+            height: 0;
+            border-left: 6px solid transparent;
+            border-right: 6px solid transparent;
+            border-top: 6px solid #aaa;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            right: 8px;
+        }
+
+        &.asc:after {
+            border-top: none;
+            border-bottom: 6px solid #aaa;
+        }
+
+        &.desc:after {
+            border-top: 6px solid #aaa;
+            border-bottom: none;
+        }
     }
-  }
 `;
 
 const StyledRow = styled.tr`
-  height: 48px;
+    height: 48px;
 `;
 
 const StyledCell = styled.td`
-  border: 2px solid #ddd;
-  padding: 8px;
-  font-size: 14px;
+    border: 2px solid #ddd;
+    padding: 8px;
+    font-size: 14px;
 `;
 
 function CredentialsTable() {
@@ -78,8 +77,10 @@ function CredentialsTable() {
         getResourceList();
     }, [getResourceList]);
 
-    const [sortField, setSortField] = useState('github_username');
-    const [sortDirection, setSortDirection] = useState('asc');
+    const [sortField, setSortField] = useState("github_username");
+    const [sortDirection, setSortDirection] = useState("asc");
+
+    const [searchTerm, setSearchTerm] = useState("");
 
     function copyToClipboard(personalAccessToken) {
         navigator.clipboard.writeText(personalAccessToken);
@@ -87,21 +88,35 @@ function CredentialsTable() {
 
     const handleSort = (field) => {
         if (field === sortField) {
-            setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+            setSortDirection(sortDirection === "asc" ? "desc" : "asc");
         } else {
             setSortField(field);
-            setSortDirection('na');
+            setSortDirection("na");
         }
     };
 
-    const sortedCredentials = resourceList.results.sort((a, b) => {
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    // filter the credentials array based on the search term
+    const filteredCredentials = resourceList.results.filter((credential) => {
+        return (
+            credential.github_username
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase()) ||
+            credential.email.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    });
+
+    const sortedCredentials = filteredCredentials.sort((a, b) => {
         const aValue = a[sortField];
         const bValue = b[sortField];
 
         if (aValue < bValue) {
-            return sortDirection === 'asc' ? -1 : 1;
+            return sortDirection === "asc" ? -1 : 1;
         } else if (aValue > bValue) {
-            return sortDirection === 'asc' ? 1 : -1;
+            return sortDirection === "asc" ? 1 : -1;
         } else {
             return 0;
         }
@@ -111,20 +126,44 @@ function CredentialsTable() {
         <div>
             <h2 style={{ textAlign: "left" }}>My GitHub Credentials</h2>
 
+            <input
+                type="text"
+                placeholder="Search by Username or Email"
+                style={{
+                    border: "2px solid #007bff",
+                    borderRadius: "4px",
+                    padding: "8px",
+                    width: "100%",
+                    boxSizing: "border-box",
+                    color: "#007bff",
+                    backgroundColor: "transparent",
+                }}
+                value={searchTerm}
+                onChange={handleSearch}
+            />
+
             <StyledTable>
                 <thead>
                     <StyledHeaderRow>
                         <StyledHeaderCell
-                            className={sortField === 'github_username' ? `sortable ${sortDirection}` : 'sortable'}
-                            onClick={() => handleSort('github_username')}
+                            className={
+                                sortField === "github_username"
+                                    ? `sortable ${sortDirection}`
+                                    : "sortable"
+                            }
+                            onClick={() => handleSort("github_username")}
                             style={{ sortField }}
                         >
                             Username
                         </StyledHeaderCell>
 
                         <StyledHeaderCell
-                            className={sortField === 'email' ? `sortable ${sortDirection}` : 'sortable'}
-                            onClick={() => handleSort('email')}
+                            className={
+                                sortField === "email"
+                                    ? `sortable ${sortDirection}`
+                                    : "sortable"
+                            }
+                            onClick={() => handleSort("email")}
                         >
                             Email
                         </StyledHeaderCell>
@@ -137,14 +176,18 @@ function CredentialsTable() {
                 <tbody>
                     {sortedCredentials.map((credential) => (
                         <StyledRow key={credential.github_username}>
-                            <StyledCell>{credential.github_username}</StyledCell>
+                            <StyledCell>
+                                {credential.github_username}
+                            </StyledCell>
                             <StyledCell>{credential.email}</StyledCell>
                             <StyledCell>
                                 <FontAwesomeIcon
                                     icon={faClipboard}
                                     title="Copy"
-                                    style={{ cursor: 'pointer' }}
-                                    onClick={() => copyToClipboard(credential.access_token)}
+                                    style={{ cursor: "pointer" }}
+                                    onClick={() =>
+                                        copyToClipboard(credential.access_token)
+                                    }
                                 />
                             </StyledCell>
                         </StyledRow>
