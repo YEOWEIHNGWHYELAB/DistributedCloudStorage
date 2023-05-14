60d01efb4304f313f339a2fe43cd30fe56c4853f
Yeo Wei Hng <yeoweihngywh@gmail.com>
2023-05-14 18:21:22 +0800

    Removed unnecessary routes

--- dcsapp/googleapi/routes/ytdirectroutes.js
+++ dcsapp/googleapi/routes/ytdirectroutes.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const ytDirectController = require('../apicaller/ytvideodirect');
 
 
 module.exports = (pool) => {
-    router.post('/ytdirect', (req, res) => {
-        ytDirectController.listVideo(req, res);
-    });
-
+    
     return router;
 }
