5dae2222a1d61c982be0158c5e2a5b9e7a9be8b6
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-05-19 14:47:12 +0800

    Nicer drag and drop region

--- dcsclient/src/Pages/Google/YouTube/Credentials/CredentialCreator.js
+++ dcsclient/src/Pages/Google/YouTube/Credentials/CredentialCreator.js
@@ -18,6 +18,7 @@ import * as Yup from "yup";
 
 function CredentialsCreator() {
     const [file, setFile] = useState(null);
+    const [isDragOver, setIsDragOver] = useState(false);
 
     const handleFileChange = (event) => {
         const uploadedFile = event.target.files[0];
@@ -28,23 +29,56 @@ function CredentialsCreator() {
         event.preventDefault();
         const uploadedFile = event.dataTransfer.files[0];
         setFile(uploadedFile);
+        setIsDragOver(false);
+    };
+
+    const handleDragEnter = (event) => {
+        event.preventDefault();
+        setIsDragOver(true);
+    };
+
+    const handleDragLeave = () => {
+        setIsDragOver(false);
+    };
+
+    const handleUpload = async () => {
+        if (file) {
+            try {
+                // const content = await parseClientSecret(file);
+                // Make an API call to your backend to store the content in the database
+                // console.log(content);
+            } catch (error) {
+                console.error("Error parsing client secret:", error);
+            }
+        }
     };
 
     return (
         <div>
+            <button onClick={handleUpload}>Upload</button>
             <input type="file" onChange={handleFileChange} />
             <div
-                onDragOver={(event) => event.preventDefault()}
+                onDragOver={handleDragEnter}
+                onDragEnter={handleDragEnter}
+                onDragLeave={handleDragLeave}
                 onDrop={handleDrop}
                 style={{
-                    width: "300px",
-                    height: "200px",
-                    border: "1px solid black",
+                    width: "100%",
+                    height: "300px",
+                    border: isDragOver ? "2px dashed #888" : "2px dashed #ccc",
+                    borderRadius: "5px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    fontSize: "18px",
+                    fontWeight: "bold",
+                    color: "#888",
+                    margin: "20px 0",
                 }}
             >
                 {file ? file.name : "Drag and drop a file here"}
             </div>
-            {/* Add a button to trigger the upload */}
         </div>
     );
 }
