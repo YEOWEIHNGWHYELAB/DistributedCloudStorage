c83df03da3d2624a360254e51b81174930cce765
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-05-22 14:42:08 +0800

    Basic welcome page

--- dcsclient/src/Pages/Dashboard/Main/MainLanding.js
+++ dcsclient/src/Pages/Dashboard/Main/MainLanding.js
@@ -1,9 +1,28 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import setHeaderToken from "../../../Contexts/SetHeaderToken";
 
 const MainLanding = () => {
+    const [user, setUser] = useState("");
+
+    const getAuthUser = () => {
+        axios
+            .get("/auth/whoami", setHeaderToken())
+            .then((res) => {
+                setUser(res.data);
+            });
+    };
+
+    useEffect(() => {
+        if (!user) {
+            getAuthUser();
+        }
+    }, [user]);
+
     return (
         <div>
-            TEST
+            <h2 style={{ textAlign: "left" }}>Welcome back {user.username}!</h2>
         </div>
     );
 };
