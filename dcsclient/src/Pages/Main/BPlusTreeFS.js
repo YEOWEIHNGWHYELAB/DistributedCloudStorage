313bde1a437b7964b746fc52fae907bf0b5e5817
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-05-26 16:11:06 +0800

    BPlusTree File System updated to support multiple files with same filename via fileID

--- dcsclient/src/Pages/Main/BPlusTreeFS.js
+++ dcsclient/src/Pages/Main/BPlusTreeFS.js
@@ -8,23 +8,28 @@ class Node {
     }
 }
 
-// BPlusTree component represents the B+Tree
 class BPlusTree {
     constructor(order) {
         this.root = new Node(true);
         this.order = order;
+        this.fileIdCounter = 1; // Unique identifier counter for files
     }
 
     // Insert a file into the tree
     insert(filename, directoryPath) {
-        const file = { filename, directoryPath };
+        const fileId = this.fileIdCounter++;
+        const file = { fileId, filename, directoryPath };
         let currentNode = this.findLeafNode(filename);
 
         // Check if the file already exists
-        const existingFileIndex = currentNode.keys.findIndex(key => key === filename);
-        if (existingFileIndex !== -1) {
-            // File already exists, update its directory path
-            currentNode.children[existingFileIndex].directoryPath = directoryPath;
+        const existingFiles = currentNode.children.filter(
+            (child) => child.filename === filename
+        );
+        const existingFile = existingFiles.find(
+            (child) => child.directoryPath === directoryPath
+        );
+        if (existingFile) {
+            // File already exists with the same filename and directory path
             return;
         }
 
@@ -39,47 +44,52 @@ class BPlusTree {
     }
 
     // Delete a file from the tree
-    delete(filename) {
+    delete(filename, fileId) {
         let currentNode = this.findLeafNode(filename);
-        const fileIndex = currentNode.keys.findIndex(key => key === filename);
+        const filesToDelete = currentNode.children.filter(
+            (child) => child.filename === filename && child.fileId === fileId
+        );
 
-        if (fileIndex === -1) {
+        if (filesToDelete.length === 0) {
             // File doesn't exist
             return;
         }
 
-        currentNode.keys.splice(fileIndex, 1);
-        currentNode.children.splice(fileIndex, 1);
+        for (let file of filesToDelete) {
+            const fileIndex = currentNode.children.indexOf(file);
+            currentNode.keys.splice(fileIndex, 1);
+            currentNode.children.splice(fileIndex, 1);
+        }
 
         if (currentNode.keys.length < Math.ceil(this.order / 2)) {
             this.adjust(currentNode);
         }
     }
 
-    // Update the directory path of a file in the tree
-    update(filename, newDirectoryPath) {
+    // Search for files with the given filename
+    search(filename) {
         let currentNode = this.findLeafNode(filename);
-        const fileIndex = currentNode.keys.findIndex(key => key === filename);
+        const files = currentNode.children.filter(
+            (child) => child.filename === filename
+        );
+        return files;
+    }
 
-        if (fileIndex === -1) {
+    // Update the directory path of a file
+    update(filename, fileId, newDirectoryPath) {
+        let currentNode = this.findLeafNode(filename);
+        const filesToUpdate = currentNode.children.filter(
+            (child) => child.filename === filename && child.fileId === fileId
+        );
+
+        if (filesToUpdate.length === 0) {
             // File doesn't exist
             return;
         }
 
-        currentNode.children[fileIndex].directoryPath = newDirectoryPath;
-    }
-
-    // Search for a file in the tree
-    search(filename) {
-        let currentNode = this.findLeafNode(filename);
-        const fileIndex = currentNode.keys.findIndex(key => key === filename);
-
-        if (fileIndex === -1) {
-            // File doesn't exist
-            return null;
+        for (let file of filesToUpdate) {
+            file.directoryPath = newDirectoryPath;
         }
-
-        return currentNode.children[fileIndex];
     }
 
     // Find the leaf node that should contain the file
@@ -88,7 +98,10 @@ class BPlusTree {
 
         while (!currentNode.isLeaf) {
             let index = 0;
-            while (index < currentNode.keys.length && filename > currentNode.keys[index]) {
+            while (
+                index < currentNode.keys.length &&
+                filename > currentNode.keys[index]
+            ) {
                 index++;
             }
             currentNode = currentNode.children[index];
@@ -99,158 +112,141 @@ class BPlusTree {
 
     // Split a node into two nodes
     split(node) {
-        const medianIndex = Math.floor(node.keys.length / 2);
-        const median = node.keys[medianIndex];
+        const midIndex = Math.floor(node.keys.length / 2);
+        const midKey = node.keys[midIndex];
+        const leftKeys = node.keys.slice(0, midIndex);
+        const rightKeys = node.keys.slice(midIndex + 1);
+        const leftChildren = node.children.slice(0, midIndex + 1);
+        const rightChildren = node.children.slice(midIndex + 1);
 
-        const leftNode = new Node(node.isLeaf);
-        const rightNode = new Node(node.isLeaf);
+        const parent = node.parent;
+        const newNode = new Node(node.isLeaf);
 
-        // Split keys and children between left and right nodes
-        leftNode.keys = node.keys.slice(0, medianIndex);
-        rightNode.keys = node.keys.slice(medianIndex + 1);
-        leftNode.children = node.children.slice(0, medianIndex + 1);
-        rightNode.children = node.children.slice(medianIndex + 1);
+        newNode.keys = rightKeys;
+        newNode.children = rightChildren;
+        newNode.parent = parent;
 
-        // Update parent-child relationship
-        for (let child of leftNode.children) {
-            child.parent = leftNode;
-        }
-        for (let child of rightNode.children) {
-            child.parent = rightNode;
-        }
+        node.keys = leftKeys;
+        node.children = leftChildren;
+        node.parent = parent;
 
-        if (node.parent === null) {
-            // Splitting the root node
-            const newRoot = new Node();
-            newRoot.keys.push(median);
-            newRoot.children.push(leftNode, rightNode);
-            this.root = newRoot;
-            leftNode.parent = newRoot;
-            rightNode.parent = newRoot;
-        } else {
-            // Insert median into parent node
-            const parent = node.parent;
-            const medianIndexParent = parent.keys.findIndex(key => key > median);
-
-            parent.keys.splice(medianIndexParent, 0, median);
-            parent.children.splice(medianIndexParent, 1, leftNode, rightNode);
+        if (parent) {
+            const childIndex = parent.children.indexOf(node);
+            parent.keys.splice(childIndex, 0, midKey);
+            parent.children.splice(childIndex + 1, 0, newNode);
 
             if (parent.keys.length > this.order) {
                 this.split(parent);
             }
+        } else {
+            const newRoot = new Node(false);
+            newRoot.keys.push(midKey);
+            newRoot.children.push(node, newNode);
+            node.parent = newRoot;
+            newNode.parent = newRoot;
+            this.root = newRoot;
         }
     }
 
     // Adjust the tree after deletion
     adjust(node) {
-        const parent = node.parent;
-
-        if (parent === null) {
-            // Adjusting the root node
-            if (node.keys.length === 0 && node.children.length === 1) {
+        if (node === this.root) {
+            if (node.keys.length === 0 && !node.isLeaf) {
                 this.root = node.children[0];
                 this.root.parent = null;
             }
             return;
         }
 
-        const index = parent.children.indexOf(node);
-        const prevSibling = parent.children[index - 1];
-        const nextSibling = parent.children[index + 1];
+        const parent = node.parent;
+        const nodeIndex = parent.children.indexOf(node);
+        const leftSibling =
+            nodeIndex > 0 ? parent.children[nodeIndex - 1] : null;
+        const rightSibling =
+            nodeIndex < parent.children.length - 1
+                ? parent.children[nodeIndex + 1]
+                : null;
 
         if (node.keys.length < Math.ceil(this.order / 2)) {
-            if (prevSibling && prevSibling.keys.length > Math.ceil(this.order / 2)) {
-                // Borrow from left sibling
-                const borrowedKey = prevSibling.keys.pop();
-                const borrowedChild = prevSibling.children.pop();
-
-                node.keys.unshift(borrowedKey);
-                node.children.unshift(borrowedChild);
-
-                if (!node.isLeaf) {
-                    borrowedChild.parent = node;
-                }
-
-                parent.keys[index - 1] = prevSibling.keys[prevSibling.keys.length - 1];
-            } else if (nextSibling && nextSibling.keys.length > Math.ceil(this.order / 2)) {
-                // Borrow from right sibling
-                const borrowedKey = nextSibling.keys.shift();
-                const borrowedChild = nextSibling.children.shift();
-
-                node.keys.push(borrowedKey);
-                node.children.push(borrowedChild);
-
-                if (!node.isLeaf) {
-                    borrowedChild.parent = node;
-                }
-
-                parent.keys[index] = nextSibling.keys[0];
-            } else if (prevSibling) {
-                // Merge with left sibling
-                const mergedKeys = prevSibling.keys.concat(node.keys);
-                const mergedChildren = prevSibling.children.concat(node.children);
-
-                prevSibling.keys = mergedKeys;
-                prevSibling.children = mergedChildren;
-
-                if (!node.isLeaf) {
-                    for (let child of node.children) {
-                        child.parent = prevSibling;
-                    }
-                }
-
-                parent.keys.splice(index - 1, 1);
-                parent.children.splice(index, 1);
-
-                if (parent.keys.length < Math.ceil(this.order / 2)) {
-                    this.adjust(parent);
-                }
-            } else if (nextSibling) {
-                // Merge with right sibling
-                const mergedKeys = node.keys.concat(nextSibling.keys);
-                const mergedChildren = node.children.concat(nextSibling.children);
-
-                node.keys = mergedKeys;
-                node.children = mergedChildren;
-
-                if (!node.isLeaf) {
-                    for (let child of nextSibling.children) {
-                        child.parent = node;
-                    }
-                }
-
-                parent.keys.splice(index, 1);
-                parent.children.splice(index + 1, 1);
-
-                if (parent.keys.length < Math.ceil(this.order / 2)) {
-                    this.adjust(parent);
+            if (
+                leftSibling &&
+                leftSibling.keys.length > Math.ceil(this.order / 2)
+            ) {
+                const borrowKey = leftSibling.keys.pop();
+                const borrowChild = leftSibling.children.pop();
+                node.keys.unshift(borrowKey);
+                node.children.unshift(borrowChild);
+                parent.keys[nodeIndex - 1] =
+                    leftSibling.keys[leftSibling.keys.length - 1];
+            } else if (
+                rightSibling &&
+                rightSibling.keys.length > Math.ceil(this.order / 2)
+            ) {
+                const borrowKey = rightSibling.keys.shift();
+                const borrowChild = rightSibling.children.shift();
+                node.keys.push(borrowKey);
+                node.children.push(borrowChild);
+                parent.keys[nodeIndex] = rightSibling.keys[0];
+            } else {
+                if (leftSibling) {
+                    const mergedNode = this.mergeNodes(leftSibling, node);
+                    this.adjust(mergedNode.parent);
+                } else if (rightSibling) {
+                    const mergedNode = this.mergeNodes(node, rightSibling);
+                    this.adjust(mergedNode.parent);
                 }
             }
         }
     }
+
+    // Merge two sibling nodes
+    mergeNodes(leftNode, rightNode) {
+        const parent = leftNode.parent;
+        const leftIndex = parent.children.indexOf(leftNode);
+        const rightIndex = parent.children.indexOf(rightNode);
+        const mergedNode = new Node(rightNode.isLeaf);
+
+        mergedNode.keys = leftNode.keys.concat(rightNode.keys);
+        mergedNode.children = leftNode.children.concat(rightNode.children);
+        mergedNode.parent = parent;
+
+        parent.keys.splice(leftIndex, 1);
+        parent.children.splice(leftIndex, 2, mergedNode);
+
+        for (let child of mergedNode.children) {
+            child.parent = mergedNode;
+        }
+
+        if (parent.keys.length === 0) {
+            this.adjust(parent);
+        }
+
+        return mergedNode;
+    }
 }
 
-// // Create an instance of the BPlusTree
-// const fileSystem = new BPlusTree(4); // Specify the order of the B+Tree
+// Create a new B+Tree with order 4
+const fileSystem = new BPlusTree(4);
 
-// // Insert files into the file system
-// fileSystem.insert("file1.txt", "/documents/");
-// fileSystem.insert("file2.txt", "/documents/");
-// fileSystem.insert("file3.txt", "/pictures/");
-// fileSystem.insert("file4.txt", "/music/");
+// Insert files into the file system
+fileSystem.insert("file1.txt", "/documents");
+fileSystem.insert("file2.txt", "/documents");
+fileSystem.insert("file1.txt", "/photos");
+fileSystem.insert("file3.txt", "/documents");
 
-// // Search for a file
-// const file1 = fileSystem.search("file1.txt");
-// console.log(file1); // Output: { filename: "file1.txt", directoryPath: "/documents/" }
+// Search for files
+const filesWithFilename1 = fileSystem.search("file1.txt");
+console.log("Files with filename 'file1.txt':", filesWithFilename1);
 
-// // Update the directory path of a file
-// fileSystem.update("file2.txt", "/new_directory/");
-// const updatedFile2 = fileSystem.search("file2.txt");
-// console.log(updatedFile2); // Output: { filename: "file2.txt", directoryPath: "/new_directory/" }
+// Update the directory path of a file
+fileSystem.update("file1.txt", 3, "/newpath");
 
-// // Delete a file
-// fileSystem.delete("file3.txt");
+// Delete a file
+fileSystem.delete("file1.txt", 1);
 
-// // Search for the deleted file
-// const deletedFile = fileSystem.search("file3.txt");
-// console.log(deletedFile); // Output: null
+// Search for files after deletion
+const filesWithFilename1Updated = fileSystem.search("file1.txt");
+console.log(
+    "Files with filename 'file1.txt' after deletion:",
+    filesWithFilename1Updated
+);
