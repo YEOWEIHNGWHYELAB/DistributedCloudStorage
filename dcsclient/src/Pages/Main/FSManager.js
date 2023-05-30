1767b705feaaba0ba8fd1b6e78e5676085e7e687
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-05-30 17:35:51 +0800

    File manager methods for frontend to be able to delete & add for files & folders

--- dcsclient/src/Pages/Main/FSManager.js
+++ dcsclient/src/Pages/Main/FSManager.js
@@ -41,6 +41,66 @@ export default class FSManager {
         }
     }
 
+    deldir(path) {
+        let curr_dir = this.root;
+
+        const pathArray = path.split('/');
+
+        for (let i = 1; i < pathArray.length - 1; i++) {
+            if (!curr_dir.directories.has(pathArray[i])) {
+                alert("Path does not exist!");
+            }
+
+            curr_dir = curr_dir.directories.get(pathArray[i]);
+        }
+
+        if (curr_dir.directories.has(pathArray[pathArray.length - 1])) {
+            curr_dir.directories.delete(pathArray[pathArray.length - 1]);
+        } else {
+            alert("Path does not exist!");
+        }
+    }
+
+    mkfile(path, fileID) {
+        if (!this.isValidDir(path)) {
+            return;
+        }
+
+        let curr_dir = this.root;
+
+        const pathArray = path.split('/');
+
+        for (let i = 1; i < pathArray.length - 1; i++) {
+            if (!curr_dir.directories.has(pathArray[i])) {
+                curr_dir.directories.set(pathArray[i], new Directory());
+            }
+
+            curr_dir = curr_dir.directories.get(pathArray[i]);
+        }
+
+        curr_dir.files.set(fileID, pathArray[pathArray.length - 1]);
+    }
+
+    delfile(path) {
+        let curr_dir = this.root;
+
+        const pathArray = path.split('/');
+
+        for (let i = 1; i < pathArray.length - 1; i++) {
+            if (!curr_dir.directories.has(pathArray[i])) {
+                alert("Invalid file path!");
+            }
+
+            curr_dir = curr_dir.directories.get(pathArray[i]);
+        }
+
+        if (curr_dir.files.has(pathArray[pathArray.length - 1])) {
+            curr_dir.files.delete(pathArray[pathArray.length - 1]);
+        } else {
+            alert("File does not exist!");
+        }
+    }
+
     ls(path_target) {
         let curr_dir = this.root;
 
@@ -62,7 +122,10 @@ export default class FSManager {
         }
 
         files_directories.push(...curr_dir.directories.keys());
-        files_directories.push(...curr_dir.files.keys());
+
+        for (let fileID of curr_dir.files.keys()) {
+            files_directories.push(curr_dir.files.get(fileID));
+        }
 
         return files_directories;
     }
