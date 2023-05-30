21903bbb2df1cf4e8ec1603981b8bd93ec74a108
Yeo Wei Hng <yeoweihngywh@gmail.com>
2023-05-30 22:30:06 +0800

    Simple tweaking that aren't fixing the file explorer

--- dcsclient/src/Pages/Main/FileExplorer.js
+++ dcsclient/src/Pages/Main/FileExplorer.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
-import "./FileExplorer.css"
+import "./FileExplorerStyle.css";
 
 const FileExplorer = () => {
     const [selectedItems, setSelectedItems] = useState([]);
@@ -11,8 +10,12 @@ const FileExplorer = () => {
     const [files, setFiles] = useState([
         { id: 1, name: 'File 1.txt', isFolder: false },
         { id: 2, name: 'File 2.png', isFolder: false },
+        { id: 3, name: 'File 3.png', isFolder: false },
+        { id: 4, name: 'File 5.png', isFolder: false },
+        { id: 5, name: 'File 4.png', isFolder: false },
     ]);
     const [isCtrlKeyPressed, setIsCtrlKeyPressed] = useState(false);
+    const [shiftStartIndex, setShiftStartIndex] = useState(null);
 
     useEffect(() => {
         const handleKeyDown = (event) => {
@@ -68,15 +71,46 @@ const FileExplorer = () => {
                 // Item not selected, add it to selectedItems
                 setSelectedItems([...selectedItems, item]);
             }
+        } else if (shiftStartIndex !== null) {
+            // Shift key is pressed
+            const startIndex = shiftStartIndex;
+            const endIndex = files.findIndex((file) => file.id === item.id);
+            const range = files.slice(Math.min(startIndex, endIndex), Math.max(startIndex, endIndex) + 1);
+
+            setSelectedItems(range);
         } else {
-            // CTRL key is not pressed, treat as single selection
-            setSelectedItems([item]);
+            // CTRL and Shift keys are not pressed, treat as single selection
+            const isSelected = selectedItems.includes(item);
+
+            if (isSelected) {
+                // Item already selected, remove it from selectedItems
+                const updatedItems = selectedItems.filter((selectedItem) => selectedItem.id !== item.id);
+                setSelectedItems(updatedItems);
+            } else {
+                // Item not selected, add it to selectedItems
+                setSelectedItems([...selectedItems, item]);
+            }
         }
+    };
+
+    const handleItemMouseDown = (e, item) => {
+        e.stopPropagation();
+
+        if (e.shiftKey) {
+            // Shift key is pressed
+            const startIndex = files.findIndex((file) => file.id === item.id);
+
+            setShiftStartIndex(startIndex);
+        }
+    };
+
+    const handleItemMouseUp = (e) => {
+        e.stopPropagation();
+        setShiftStartIndex(null);
     };
 
     return (
         <div>
-            <h2>File Explorer</h2>
             <div className="file-explorer">
                 <div className="folders">
                     <h3>Folders</h3>
@@ -93,6 +127,8 @@ const FileExplorer = () => {
                                     key={item.id}
                                     className={`item ${selectedItems.includes(item) ? 'selected' : ''}`}
                                     onClick={(e) => handleItemSelection(e, item)}
+                                    onMouseDown={(e) => handleItemMouseDown(e, item)}
+                                    onMouseUp={handleItemMouseUp}
                                 >
                                     {item.name}
                                 </div>
@@ -102,13 +138,15 @@ const FileExplorer = () => {
                 </div>
                 <div className="files">
                     <h3>Files</h3>
-                    {files.map((file) => (
+                    {files.map((file, index) => (
                         <div
                             key={file.id}
                             className={`item ${selectedItems.includes(file) ? 'selected' : ''}`}
                             draggable
                             onDragStart={(e) => handleFileDragStart(e, file.id)}
                             onClick={(e) => handleItemSelection(e, file)}
+                            onMouseDown={(e) => handleItemMouseDown(e, file)}
+                            onMouseUp={handleItemMouseUp}
                         >
                             {file.name}
                         </div>
