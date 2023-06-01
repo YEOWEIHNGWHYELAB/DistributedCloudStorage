6c53315c384188361677ef32c9ed8888b9ab5b1f
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-06-01 14:04:55 +0800

    FSManager tested with new renaming method

--- dcsclient/src/Pages/Main/FSManager.js
+++ dcsclient/src/Pages/Main/FSManager.js
@@ -5,14 +5,13 @@ class Directory {
     }
 }
 
-
 export default class FSManager {
     constructor() {
         this.root = new Directory();
     }
 
     isValidDir(path) {
-        const pathArray = path.split('/');
+        const pathArray = path.split("/");
 
         for (let i = 1; i < pathArray.length; i++) {
             if (pathArray[i] == "") {
@@ -23,6 +22,30 @@ export default class FSManager {
         return true;
     }
 
+    filefoldermod(curr_dir, pathArray, isDir, isDelete, newName, fileID) {
+        if (isDir) {
+            if (curr_dir.directories.has(pathArray[pathArray.length - 1])) {
+                curr_dir.directories.delete(pathArray[pathArray.length - 1]);
+
+                if (!isDelete) {
+                    curr_dir.directories.set(newName, new Directory());
+                }
+            } else {
+                return console.log("Path does not exist!");
+            }
+        } else {
+            if (curr_dir.files.has(fileID)) {
+                curr_dir.files.delete(fileID);
+
+                if (!isDelete) {
+                    curr_dir.files.set(fileID, newName);
+                }
+            } else {
+                return console.log("File does not exist!");
+            }
+        }
+    }
+
     mkdir(path) {
         if (!this.isValidDir(path)) {
             return;
@@ -30,7 +53,7 @@ export default class FSManager {
 
         let curr_dir = this.root;
 
-        const pathArray = path.split('/');
+        const pathArray = path.split("/");
 
         for (let i = 1; i < pathArray.length; i++) {
             if (!curr_dir.directories.has(pathArray[i])) {
@@ -41,24 +64,40 @@ export default class FSManager {
         }
     }
 
-    deldir(path) {
+    renamedir(path, folderName) {
+        if (!this.isValidDir(path)) {
+            return;
+        }
+
         let curr_dir = this.root;
 
-        const pathArray = path.split('/');
+        const pathArray = path.split("/");
 
         for (let i = 1; i < pathArray.length - 1; i++) {
             if (!curr_dir.directories.has(pathArray[i])) {
-                alert("Path does not exist!");
+                return console.log("Path does not exist!");
             }
 
             curr_dir = curr_dir.directories.get(pathArray[i]);
         }
 
-        if (curr_dir.directories.has(pathArray[pathArray.length - 1])) {
-            curr_dir.directories.delete(pathArray[pathArray.length - 1]);
-        } else {
-            alert("Path does not exist!");
+        this.filefoldermod(curr_dir, pathArray, true, false, folderName, null);
+    }
+
+    deldir(path) {
+        let curr_dir = this.root;
+
+        const pathArray = path.split("/");
+
+        for (let i = 1; i < pathArray.length - 1; i++) {
+            if (!curr_dir.directories.has(pathArray[i])) {
+                return console.log("Path does not exist!");
+            }
+
+            curr_dir = curr_dir.directories.get(pathArray[i]);
         }
+
+        this.filefoldermod(curr_dir, pathArray, true, true, null, null);
     }
 
     mkfile(path, fileID) {
@@ -68,7 +107,7 @@ export default class FSManager {
 
         let curr_dir = this.root;
 
-        const pathArray = path.split('/');
+        const pathArray = path.split("/");
 
         for (let i = 1; i < pathArray.length - 1; i++) {
             if (!curr_dir.directories.has(pathArray[i])) {
@@ -81,24 +120,36 @@ export default class FSManager {
         curr_dir.files.set(fileID, pathArray[pathArray.length - 1]);
     }
 
-    delfile(path) {
+    renamefile(path, fileID, fileName) {
         let curr_dir = this.root;
 
-        const pathArray = path.split('/');
+        const pathArray = path.split("/");
 
         for (let i = 1; i < pathArray.length - 1; i++) {
             if (!curr_dir.directories.has(pathArray[i])) {
-                alert("Invalid file path!");
+                return console.log("Invalid file path!");
             }
 
             curr_dir = curr_dir.directories.get(pathArray[i]);
         }
 
-        if (curr_dir.files.has(pathArray[pathArray.length - 1])) {
-            curr_dir.files.delete(pathArray[pathArray.length - 1]);
-        } else {
-            alert("File does not exist!");
+        this.filefoldermod(curr_dir, pathArray, false, false, fileName, fileID);
+    }
+
+    delfile(path, fileID) {
+        let curr_dir = this.root;
+
+        const pathArray = path.split("/");
+
+        for (let i = 1; i < pathArray.length - 1; i++) {
+            if (!curr_dir.directories.has(pathArray[i])) {
+                return console.log("Invalid file path!");
+            }
+
+            curr_dir = curr_dir.directories.get(pathArray[i]);
         }
+
+        this.filefoldermod(curr_dir, pathArray, false, true, null, fileID);
     }
 
     ls(path_target) {
@@ -106,8 +157,8 @@ export default class FSManager {
 
         const files_directories = [];
 
-        if (path_target !== '/') {
-            const path_array = path_target.split('/');
+        if (path_target !== "/") {
+            const path_array = path_target.split("/");
 
             for (let i = 1; i < path_array.length - 1; i++) {
                 curr_dir = curr_dir.directories.get(path_array[i]);
@@ -117,7 +168,9 @@ export default class FSManager {
                 files_directories.push(path_array[path_array.length - 1]);
                 return files_directories;
             } else {
-                curr_dir = curr_dir.directories.get(path_array[path_array.length - 1]);
+                curr_dir = curr_dir.directories.get(
+                    path_array[path_array.length - 1]
+                );
             }
         }
 
@@ -131,7 +184,6 @@ export default class FSManager {
     }
 }
 
-
 // Mini Test
 /*
 let fsManager = new FSManager();
@@ -139,7 +191,20 @@ let fsManager = new FSManager();
 fsManager.mkdir("/Documents/Photos");
 fsManager.mkdir("/Notes");
 fsManager.mkdir("/Documents/lalala");
-
+fsManager.mkfile("/Documents/note.txt", "gh_4");
+fsManager.mkfile("/Documents/video.mp4", "yt_inwg");
 console.log(fsManager.ls("/"));
 console.log(fsManager.ls("/Documents"));
+
+fsManager.delfile("/Documents/video.mp4", "yt_inwg");
+fsManager.renamefile("/Documents/note.txt", "gh_4", "test.txt");
+console.log(fsManager.ls("/Documents"));
+
+fsManager.deldir("/Documents");
+fsManager.renamedir("/Notes", "Test");
+fsManager.mkfile("/video.mp4", "yt_inwg");
+console.log(fsManager.ls("/"));
+
+fsManager.deldir("/Test");
+console.log(fsManager.ls("/"));
 */
